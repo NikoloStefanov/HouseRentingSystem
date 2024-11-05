c963786a8c521be0d44681b0fd62bbd014b51006
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2024-11-05 16:34:10 +0200

    start

---
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationsServices(this IServiceCollection service )
+        {
+            return service;
+        }
+        public static IServiceCollection AddApplicationsDbContext(this IServiceCollection services, IConfiguration confic)
+        {
+            var connectionString = confic.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+        public static IServiceCollection AddApplicationsIdentity(this IServiceCollection services,IConfiguration confic)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+            return services;
+        }
+    }
+
+}
