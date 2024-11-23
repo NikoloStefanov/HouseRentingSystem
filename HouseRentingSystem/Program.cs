e7a4a1661b70d6956cb5be9ac4d5dbd90b1dc8b3
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2024-11-23 11:52:43 +0200

    login and register update

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -1,13 +1,17 @@
 using HouseRentingSystem.Infrastructurea.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationsDbContext(builder.Configuration);
 builder.Services.AddApplicationsIdentity(builder.Configuration);
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+/*builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
 
 
 builder.Services.AddControllersWithViews(options => 
