87f33e7587f99fc50d9efdcf427c2d644a60e486
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2024-11-23 12:59:47 +0200

    add logout

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -4,6 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+/*builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
 
 builder.Services.AddApplicationsDbContext(builder.Configuration);
 builder.Services.AddApplicationsIdentity(builder.Configuration);
